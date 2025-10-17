5934e99e16340406f3b24551278c7c21967274bd
beibanezv <be.ibanezv@duocuc.cl>
2025-10-16 23:33:56 -0300

    ultimos ajustes

--- micarrito/src/__tests__/Boton.test.js
+++
@@ -1,9 +0,0 @@
-import Boton from '../componentes/atomos/Boton';
-
-describe('Boton', () => {
-  it('es una función que produce un elemento button', () => {
-    const el = Boton({ children: 'Click' });
-    expect(el).toBeDefined();
-    expect(el.type).toBe('button');
-  });
-});
